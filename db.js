390c75071d515f1c92b4ddc0027e1012892de0f0
jainish koladiya <155936377+jainish2222@users.noreply.github.com>
2025-05-11 21:48:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbconnect = async () => {
   try {
-      await mongoose.connect("mongodb+srv://jainishkoladiya33:UirfbhJ6RXOjWvY6@cluster0.ejj05.mongodb.net/auth");
+      await mongoose.connect("mongodb+srv://jainishkoladiya33:K2fYn6J2w3RTxQT6@cluster0.clsj52w.mongodb.net/auth");
       console.log('Connected to the database');
   } catch (error) {
       console.error('Error connecting to the database:', error.message);
